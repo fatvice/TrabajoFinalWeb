0e1152e6f103fea765cf4f13b80c5da07916ed0f
fatvice <vicecapa2000@gmail.com>
2021-08-08 03:56:38 -0400

    js de registrar_pago_deuda y registrar_reclamo_sugerencia

--- web/covadonga/public/js/registrar_pago_deuda.js
+++ web/covadonga/public/js/registrar_pago_deuda.js
@@ -1,19 +1,36 @@
 //Ingreso de datos de la información
-document.querySelector("#registrar-btn").addEventListener("click", ()=>{
+document.querySelector("#registrar-btn").addEventListener("click", async()=>{
     let usuario = document.querySelector("#usuario-select").value.trim();
     let tipoInfo = document.querySelector("#opcion-select").value.trim();
     let monto = +document.querySelector("#monto-txt").value;
     let fecha = document.querySelector("#fecha-txt").value;
+    let errores = [];
 
-    let info={};
-    info.usuario=usuario;
-    info.tipoInfo=tipoInfo;
-    info.monto=monto;
-    info.fecha=fecha;
-    console.log(info);
-    if(tipoInfo=="1"){
-        Swal.fire("Pago registrado");
+    if(monto <= 0){
+        errores.push("El monto ingresado debe ser mayor a 0");
+    }
+    if(fecha == ""){
+        errores.push("Debe ingresar una fecha");
+    }
+
+    if(errores.length == 0){
+        let info={};
+        info.usuario=usuario;
+        info.tipoInfo=tipoInfo;
+        info.monto=monto;
+        info.fecha=fecha;
+        if(tipoInfo=="1"){
+            let resultado = await crearPago(info);
+            Swal.fire("Pago registrado");
+        }else{
+            let resultado = await crearDeuda(info);
+            Swal.fire("Deuda registrada");
+        }
     }else{
-        Swal.fire("Deuda registrada");
-    }     
+        Swal.fire({
+            title:"Errores de validación",
+            icon:"warning",
+            html:errores.join("<br />")
+        });
+    }
 });
--- web/covadonga/public/js/registrar_reclamo_sugerencia.js
+++ web/covadonga/public/js/registrar_reclamo_sugerencia.js
@@ -1,17 +1,30 @@
 //Ingreso de datos de la información
-document.querySelector("#registrar-btn").addEventListener("click", ()=>{
+document.querySelector("#registrar-btn").addEventListener("click", async()=>{
     let usuario = document.querySelector("#usuario-select").value.trim();
     let tipoInfo = document.querySelector("#opcion-select").value.trim();
     let texto = document.querySelector("#texto-txt").value.trim();
+    let errores = [];
 
-    let info={};
-    info.usuario=usuario;
-    info.tipoInfo=tipoInfo;
-    info.texto=texto;
-    console.log(info);
-    if(tipoInfo=="1"){
-        Swal.fire("Reclamo registrado");
+    if(texto.length == 0){
+        errores.push("Debe ingresar texto");
+    }
+
+    if(errores.length == 0){
+        let info={};
+        info.usuario=usuario;
+        info.tipoInfo=tipoInfo;
+        info.texto=texto;
+        let resultado = await crearReclamoSugerencia(info);
+        if(tipoInfo=="1"){
+            Swal.fire("Reclamo registrado");
+        }else{
+            Swal.fire("Sugerencia registrada");
+        }
     }else{
-        Swal.fire("Sugerencia registrada");
-    }    
+        Swal.fire({
+            title:"Errores de validacion",
+            icon:"warning",
+            html:errores.join("<br />")
+        });
+    }
 });
